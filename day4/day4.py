3d720714caf7e61eab6b183876803b40020ce2dd
Teddy <teodor.adolfsson@gmail.com>
2022-03-21 03:44:31 +0100

    work on day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,5 +1,38 @@
-with open (".\input4.txt", "r") as f:
-    draws = f.readline().split(",")
-    boards = []
+draws = None
+board = []
+boards = []
 
-    print(draws)   
+for line in open (".\input4.txt", "r"):
+    line = line.strip()
+
+    #filter out the first line
+    if draws is None:
+        draws = [int(x) for x in line.split(",")]
+
+    #parse the boards
+    else: 
+        if line:
+            board.append([int(x) for x in line.split()])
+        else:
+            if board:
+                boards.append(board)
+                board = []
+boards.append(board)
+
+#for every number drawn
+for draw in draws:
+    #check if the number is on a board and mark it true
+    for board in boards:
+        if draw in board:
+            board[draw] = True
+    
+    #check for winning boards
+    for board in boards:
+
+    
+        for i in range(5):
+            for j in range(5):
+                break
+                
+print(draws)
+print(boards)
