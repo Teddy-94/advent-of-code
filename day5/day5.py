6e8b3aeed1b256ae8b683a328da864f96049ce2a
Teddy <teodor.adolfsson@gmail.com>
2022-03-22 04:13:17 +0100

    complete day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,4 +1,41 @@
-#At how many points do at least two lines overlap?
+from collections import defaultdict
+
+from numpy import deg2rad
+
+points = defaultdict(int) 
+points2 = defaultdict(int) 
 
 with open (".\input5.txt", "r") as f:
         d = f.read().strip().splitlines()
+
+        #parse data
+        for line in d:
+            p1, p2 = line.split("->")
+            x1, y1 = tuple(map(int, p1.split(",")))
+            x2, y2 = tuple(map(int, p2.split(",")))
+
+            dx = x2-x1
+            dy = y2-y1
+
+            for i in range(max(abs(dx), abs(dy)) + 1):
+                x = x1 + (1 if dx > 0 else (-1 if dx < 0 else 0))*i
+                y = y1 + (1 if dy > 0 else (-1 if dy < 0 else 0))*i                    
+
+                if dx == 0 or dy == 0:
+                    points[(x,y)] += 1
+                points2[(x,y)] +=1
+                
+count = 0
+for n in points:
+    if points[n] > 1:
+        count +=1
+print(count)
+
+count2 = 0
+for n in points2:
+    if points2[n] > 1:
+        count2 +=1
+print(count + count2)
+
+print(len([k for k in points if points[k]>1]))
+print(len([k for k in points2 if points2[k]>1]))
