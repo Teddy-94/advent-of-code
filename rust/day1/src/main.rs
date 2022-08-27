8a92077a4575d0b98720735e09713f061926dd77
Teddy <teodor.adolfsson@gmail.com>
2022-08-27 19:00:02 +0300

    work on rust day1

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn read_data() -> Vec<usize>{
+    let values = fs::read_to_string("/day1/day1.txt").expect("Couldn't read data");
+    values
+        .split('\n')
+        .filter_map(|s|s.parse::<usize>().ok())
+        .collect()
 }
