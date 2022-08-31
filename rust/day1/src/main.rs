c9f674287a7705ee1c8f68dfffe4384002fec32d
Teddy <teodor.adolfsson@gmail.com>
2022-08-31 18:38:33 +0300

    rust day1 part a

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,13 +1,22 @@
-use std::fs;
-
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("../../input/day1.txt").expect("can't read file");
+    let nums: Vec<i32> = input
+        .trim()
+        .lines()
+        .map(|n| n.parse::<i32>().expect("some error"))
+        .collect();
+
+    part_a(&nums);
 }
 
-fn read_data() -> Vec<usize>{
-    let values = fs::read_to_string("/day1/day1.txt").expect("Couldn't read data");
-    values
-        .split('\n')
-        .filter_map(|s|s.parse::<usize>().ok())
-        .collect()
+fn part_a(nums: &Vec<i32>) {
+    
+    let mut count:i32 = 0;
+
+    for i in 1..nums.len() {
+        if nums[i] > nums[i - 1] {
+            count += 1;
+        }
+    }
+    print!("{}", count.to_string());
 }
