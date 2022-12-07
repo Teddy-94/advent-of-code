75be0415976014e7d3a891a8021f73e88e861de3
Teddy <teodor.adolfsson@gmail.com>
2022-12-07 21:40:53 +0200

    day10

--- 2021/python/day10/day10.py
+++ 2021/python/day10/day10.py
@@ -1,4 +1,92 @@
 with open("..\..\input\day10.txt") as f:
     d = f.readlines()
 
-    
+
+def part1():
+    score = {
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137
+    }
+
+    closingTag = {
+        "(": ")",
+        "[": "]",
+        "{": "}",
+        "<": ">",
+    }
+
+    total = 0
+
+    for line in d:
+        stack = []
+        ok = True
+        for char in line:
+            if not ok:
+                break
+            for openingTag in closingTag:
+                if char == openingTag:
+                    stack.append(char)
+                elif char == closingTag[openingTag]:
+                    if openingTag == stack[-1]:
+                        stack.pop()
+                    else:
+                        print(stack)
+                        print(
+                            f"this looks like an unopened closing item --> {char} adding {score[char]}")
+                        total = total + score[char]
+                        print(f"total score is now {total}")
+                        ok = False
+                        break
+    return(total)
+
+def part2():
+    score = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4
+    }
+
+    closingTag = {
+        "(": ")",
+        "[": "]",
+        "{": "}",
+        "<": ">",
+    }
+
+    totalScore = []
+
+    for line in d:
+        lineScore = 0
+        stack = []
+        ok = True
+        for idx, char in enumerate(line):
+            if not ok:
+                break
+            for openingTag in closingTag:
+                if char == openingTag:
+                    stack.append(char)
+                elif char == closingTag[openingTag]:
+                    if openingTag == stack[-1]:
+                        stack.pop()
+                    else:
+                        # this is a corrupted line
+                        ok = False
+                        
+            if ok and idx==len(line)-1:
+                # this is an unfinished line, needs to be completed and scored
+                print(stack)
+                print(lineScore)
+                for i in range(len(stack)):
+                    for openingTag in closingTag:
+                        if openingTag == stack[len(stack)-1-i]:
+                            lineScore = lineScore*5+score[closingTag[openingTag]]
+                print(lineScore)
+                totalScore.append(lineScore)
+    return(sorted(totalScore)[len(totalScore)//2])
+
+print(#part1(),
+    part2()
+)
