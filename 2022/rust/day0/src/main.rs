88b8ab33ea7881b1558610351ae2717390f0e7cf
Teddy <teodor.adolfsson@gmail.com>
2022-12-10 12:54:34 +0200

    template files (day0)

---
+++ 2022/rust/day0/src/main.rs
@@ -0,0 +1,42 @@
+// template
+fn main() {
+    let input_path = format!("../../input/{}.txt", env!("CARGO_PKG_NAME"));
+    let input: String = std::fs::read_to_string(input_path).expect("can't read file");
+    let row: Vec<&str> = input.split("\r\n").collect::<Vec<&str>>();
+
+    println!(
+        "part 1 {:?},
+        \npart 2 {}",
+        part_1(&row),
+        part_2(&row)
+    )
+}
+
+fn part_1(row: &Vec<&str>) -> i32 {
+    
+    return 0;
+}
+
+fn part_2(row: &Vec<&str>) -> i32 {
+    
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part_1() {
+        let input_path = format!("../../input/{}_test.txt", env!("CARGO_PKG_NAME"));
+        let input: String = std::fs::read_to_string(input_path).expect("can't read file");
+        let row: Vec<&str> = input.split("\r\n").collect::<Vec<&str>>();
+        assert_eq!(part_1(&row),0);
+    }
+    #[test]
+    fn test_part_2() {
+        let input_path = format!("../../input/{}_test.txt", env!("CARGO_PKG_NAME"));
+        let input: String = std::fs::read_to_string(input_path).expect("can't read file");
+        let row: Vec<&str> = input.split("\r\n").collect::<Vec<&str>>();
+        assert_eq!(part_2(&row),0);
+    }
+}
