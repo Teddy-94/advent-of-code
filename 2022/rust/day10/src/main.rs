e77b3398aeb26232ed9d14668ce97ae409f9a115
Teddy <teodor.adolfsson@gmail.com>
2023-03-02 03:39:52 +0200

    part 1 and 2

--- 2022/rust/day10/src/main.rs
+++ 2022/rust/day10/src/main.rs
@@ -3,58 +3,54 @@ fn main() {
     let input: String = std::fs::read_to_string(input_path).expect("can't read file");
     let row: Vec<&str> = input.split("\r\n").collect::<Vec<&str>>();
 
-    println!("part 1 {}\npart 2 {}", part_1(&row), part_2(&row))
+    println!("part 1 {}", part_1_and_2(&row))
 }
 
-fn part_1(row: &Vec<&str>) -> i32 {
-    let (mut instruction, mut value) = ("", 0);
-    let mut cycle = 0;
-    let mut x_register = 0;
+fn part_1_and_2(row: &Vec<&str>) -> i32 {
+    let mut current_cycle = 0;
+    let mut x_register = 1;
     let mut sum_signal_strenght = 0;
-    fn signal_strenght_modulator(x_register: &i32, current_cycle: &i32) -> i32 {
-        let signal_strenght = x_register * current_cycle;
-        return signal_strenght;
-    }
-
-    fn increase_cycle(
-        count: i32,
-        mut current_cycle: i32,
-        x_register: i32,
-        mut sum_signal_strenght: i32,
-    ) -> usize {
-        for _ in 0..count {
-            current_cycle += 1;
-            if current_cycle == 20 {
-                sum_signal_strenght += signal_strenght_modulator(&x_register, &current_cycle)
-            }
-        }
-        return 1;
-    }
 
     for r in row {
+        current_cycle += 1;
+        sum_signal_strenght += update_signal_strength(&x_register, &current_cycle);
+
+        draw_crt(&current_cycle, &x_register);
+
         if r.contains("addx") {
-            let (a, b) = r.split_once(' ').unwrap();
-            instruction = a;
-            value = b.parse().unwrap();
-        }
-        match instruction {
-            "noop" => {
-                cycle += 1;
-            }
-            "addx" => {
-                cycle += 2;
-                //x_register += value;
-            }
-            _ => println!("Whoah!"),
+            let (_, b) = r.split_once(' ').unwrap();
+            let value: i32 = b.parse().unwrap();
+
+            current_cycle += 1;
+            draw_crt(&current_cycle, &x_register);
+
+            sum_signal_strenght += update_signal_strength(&x_register, &current_cycle);
+            x_register += value;
         }
 
-        println!(" {:?} {:?} ", r, value);
+        fn update_signal_strength(x_register: &i32, current_cycle: &i32) -> i32 {
+            if [20, 60, 100, 140, 180, 220].contains(&current_cycle) {
+                let signal_strength_delta = x_register * current_cycle;
+                return signal_strength_delta;
+            }
+            return 0;
+        }
+
+        fn draw_crt(current_cycle: &i32, x_register: &i32) {
+            let sprite_pos = [x_register - 1, x_register + 0, x_register + 1];
+            let current_cycle_mod_40 = current_cycle % 40 - 1;
+
+            if current_cycle_mod_40 == 0 {
+                println!();
+            }
+            if sprite_pos.contains(&current_cycle_mod_40) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
     }
-    println!(" cycle {:?} register {:?} ", cycle, x_register);
-    return 0;
-}
-fn part_2(row: &Vec<&str>) -> i32 {
-    return 0;
+    return sum_signal_strenght;
 }
 
 #[cfg(test)]
@@ -65,13 +61,6 @@ mod tests {
         let input_path = format!("../../input/{}_test.txt", env!("CARGO_PKG_NAME"));
         let input: String = std::fs::read_to_string(input_path).expect("can't read file");
         let row: Vec<&str> = input.split("\r\n").collect::<Vec<&str>>();
-        assert_eq!(part_1(&row), 13140);
-    }
-    #[test]
-    fn test_part_2() {
-        let input_path = format!("../../input/{}_test.txt", env!("CARGO_PKG_NAME"));
-        let input: String = std::fs::read_to_string(input_path).expect("can't read file");
-        let row: Vec<&str> = input.split("\r\n").collect::<Vec<&str>>();
-        assert_eq!(part_2(&row), 0);
+        assert_eq!(part_1_and_2(&row), 13140);
     }
 }
